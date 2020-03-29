fe6035a06bcd760382282bfe39d52553312ea5ee
DevHunter <32465888+DevHunterYZ@users.noreply.github.com>
2020-03-29 21:22:08 +0300

    Create Vİsual q&a.py

---
+++ Vİsual q&a.py
@@ -0,0 +1,31 @@
+# Import Keras 
+from keras.layers import Conv2D, MaxPooling2D, Flatten
+from keras.layers import Input, LSTM, Embedding, Dense
+from keras.models import Model, Sequential
+
+# Define CNN for Image Input
+vision_model = Sequential()
+vision_model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(224, 224, 3)))
+vision_model.add(Conv2D(64, (3, 3), activation='relu'))
+vision_model.add(MaxPooling2D((2, 2)))
+vision_model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+vision_model.add(Conv2D(128, (3, 3), activation='relu'))
+vision_model.add(MaxPooling2D((2, 2)))
+vision_model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+vision_model.add(Conv2D(256, (3, 3), activation='relu'))
+vision_model.add(Conv2D(256, (3, 3), activation='relu'))
+vision_model.add(MaxPooling2D((2, 2)))
+vision_model.add(Flatten())
+
+image_input = Input(shape=(224, 224, 3))
+encoded_image = vision_model(image_input)
+
+# Define RNN for language input
+question_input = Input(shape=(100,), dtype='int32')
+embedded_question = Embedding(input_dim=10000, output_dim=256, input_length=100)(question_input)
+encoded_question = LSTM(256)(embedded_question)
+
+# Combine CNN and RNN to create the final model
+merged = keras.layers.concatenate([encoded_question, encoded_image])
+output = Dense(1000, activation='softmax')(merged)
+vqa_model = Model(inputs=[image_input, question_input], outputs=output)
